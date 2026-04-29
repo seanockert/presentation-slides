39b47ca4e1692b44c21cb8ebdbf3cbee49508f84
Sean Ockert <seanockert@gmail.com>
2026-04-29 13:05:30 +1000

    Add Make State Great Again slides

---
+++ make-state-great-again/service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'slides-001';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/project-setup-titles.png',
+  '/unmod-results-titles.png',
+  '/thug.jpg',
+  '/tumbleweed.gif',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) return cachedResponse;
+
+        return fetch(event.request.clone())
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => cache.put(event.request, responseToCache));
+
+            return response;
+          })
+          .catch((error) => {
+            if (event.request.mode === 'navigate') {
+              return caches.match('/index.html');
+            }
+            throw error;
+          });
+      })
+  );
+});
